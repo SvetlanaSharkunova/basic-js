767e5f7f9a9baac02919eead31d7c72c0bbe019a
ssharkunova <sharkunova@yandex.ru>
2021-03-30 22:57:55 +0300

    fix: correct function for simple-chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -2,31 +2,33 @@ const CustomError = require("../extensions/custom-error");
 
 const chainMaker = {
   chainArr: [],
-  getLength() {
-    return chainMaker.chainArr.length;
+  getLength() {    
+    return this.chainArr.length;
   },
   addLink(value) {
     if (value === undefined) {
-      chainMaker.chainArr.push('( )');
+      this.chainArr.push('( )');
     } else {
-      chainMaker.chainArr.push('( ' + value + ' )');
+      this.chainArr.push('( ' + value + ' )');
     }    
-    return chainMaker.chainArr;
+    return this;
   },
   removeLink(position) {
-    if (!Number.isInteger(position) || (position > this.getLength - 1) || (isNan(position))) {
-      chainMaker.chainArr = [];
+    if (typeof position === "number") {
+      this.chainArr = [];
         throw new Error();
         } else {
-          return chainMaker.chainArr.splice(position, 1);
+           this.chainArr.splice(position, 1);
+           return this;
     }    
   },
   reverseChain() {
-    return chainMaker.chainArr.reverse();
+    this.chainArr.reverse();
+    return this; 
   },
   finishChain() {
     let chainFinal = chainMaker.chainArr.join("~~");
-    chainMaker.chainArr = [];
+    this.chainArr = [];
     return chainFinal;
   }
 };
